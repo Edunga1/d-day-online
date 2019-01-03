20def886fa2403cf3330f93ee64669c42e7db369
edunga1 <goonr21@gmail.com>
2019-01-03 13:15:42 +0900

    2019-01-03 13:15:42

---
+++ precache-manifest.031fdf2a11def8f14e7e2853043e1b84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab983c453ed5714f567c",
+    "url": "/d-day-online/static/js/runtime~main.ab983c45.js"
+  },
+  {
+    "revision": "e4e05e9859e7ce5ee4df",
+    "url": "/d-day-online/static/js/main.e4e05e98.chunk.js"
+  },
+  {
+    "revision": "10c5b14cc29007a9b013",
+    "url": "/d-day-online/static/js/1.10c5b14c.chunk.js"
+  },
+  {
+    "revision": "e4e05e9859e7ce5ee4df",
+    "url": "/d-day-online/static/css/main.7abff1e1.chunk.css"
+  },
+  {
+    "revision": "b1edbb0783a9b64f4e170d7795734e30",
+    "url": "/d-day-online/index.html"
+  }
+];
